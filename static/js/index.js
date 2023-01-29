3f2ea69d5e4b3d379064ff35a00862ab0e981e9f
epicnoxious <jappreet16042002@gmail.com>
2023-01-29 11:20:04 +0530

    Initial commit with index, navbar, footer design

---
+++ static/js/index.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector(".burger");
+const slider = document.querySelector(".slider");
+const place = document.querySelector(".place");
+const nav__head = document.querySelector(".nav__head");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
+const menuitems = document.querySelectorAll(".menu-item");
+burger.addEventListener("click", (e) => {
+  slider.classList.toggle("slider-focus");
+  nav__head.classList.toggle("nav__head-focus");
+  place.classList.toggle("place-focus");
+  line1.classList.toggle("line1-focus");
+  line2.classList.toggle("line2-focus");
+  menuitems.forEach((menuitem) => {
+    menuitem.classList.toggle("menu-item-focus");
+  });
+  console.log("hey");
+});
