d988c65614ff16c5a56dcae81fa93ae71481e8d8
epicnoxious <jappreet16042002@gmail.com>
2023-01-30 13:04:26 +0530

    +cursors svg + blogs page

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,11 @@
-const burger = document.querySelector(".burger");
+const lines = document.querySelector(".lines");
 const slider = document.querySelector(".slider");
 const place = document.querySelector(".place");
 const nav__head = document.querySelector(".nav__head");
 const line1 = document.querySelector(".line1");
 const line2 = document.querySelector(".line2");
 const menuitems = document.querySelectorAll(".menu-item");
-burger.addEventListener("click", (e) => {
+lines.addEventListener("click", (e) => {
   slider.classList.toggle("slider-focus");
   nav__head.classList.toggle("nav__head-focus");
   place.classList.toggle("place-focus");
