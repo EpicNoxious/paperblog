3279cb7f6e7080b4948ab61bbe033a435a59722d
epicnoxious <jappreet16042002@gmail.com>
2023-01-30 18:13:13 +0530

    +Fix navbar orientation on blogs

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,26 @@
-const lines = document.querySelector(".lines");
+const nav = document.querySelector("nav");
 const slider = document.querySelector(".slider");
 const place = document.querySelector(".place");
 const nav__head = document.querySelector(".nav__head");
+const lines = document.querySelector(".lines");
 const line1 = document.querySelector(".line1");
 const line2 = document.querySelector(".line2");
 const menuitems = document.querySelectorAll(".menu-item");
+
+// change slider on particular url
+if (window.location.href.indexOf("blogs.html") > -1) {
+  nav.classList.add("nav-blogs");
+  slider.classList.add("slider-blogs");
+} else {
+  nav.classList.remove("nav-blogs");
+  slider.classList.remove("slider-blogs");
+}
 lines.addEventListener("click", (e) => {
-  slider.classList.toggle("slider-focus");
+  if (window.location.href.indexOf("blogs.html") > -1) {
+    slider.classList.toggle("slider-blogs-focus");
+  } else {
+    slider.classList.toggle("slider-focus");
+  }
   nav__head.classList.toggle("nav__head-focus");
   place.classList.toggle("place-focus");
   line1.classList.toggle("line1-focus");
