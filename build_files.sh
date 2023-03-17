f1102ff67f3f44b31371188f38705011c406aa47
epicnoxious <jappreet16042002@gmail.com>
2023-03-18 00:40:09 +0530

    +Inital

---
+++ build_files.sh
@@ -0,0 +1,11 @@
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+
+
+echo "MAKE MIGRATIONS"
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "COLLECT STATIC"
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"
