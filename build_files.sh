452426273682a064a4e3a3e8df11eeb2d94237eb
epicnoxious <jappreet16042002@gmail.com>
2023-03-18 12:08:43 +0530

    update build

--- build_files.sh
+++ build_files.sh
@@ -2,9 +2,9 @@ echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
 
 
-# echo "MAKE MIGRATIONS"
-# python3.9 manage.py makemigrations blogs
-# python3.9 manage.py migrate blogs
+echo "MAKE MIGRATIONS"
+python3.9 manage.py makemigrations blogs
+python3.9 manage.py migrate blogs
 
 echo "COLLECT STATIC"
 python3.9 manage.py collectstatic --noinput --clear
