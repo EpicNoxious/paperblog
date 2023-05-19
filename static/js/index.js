04b062e67245e6555af47b311cf5ab20279d96d9
epicnoxious <jappreet16042002@gmail.com>
2023-05-19 09:31:18 +0530

    bug fix lenis scroll mobile view

--- static/js/index.js
+++ static/js/index.js
@@ -7,9 +7,10 @@ const lines = document.querySelector(".lines");
 const line1 = document.querySelector(".line1");
 const line2 = document.querySelector(".line2");
 const menuitems = document.querySelectorAll(".menu-item");
-const lenis = new Lenis({
-  duration: 1.3,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
 });
 
 function raf(time) {
